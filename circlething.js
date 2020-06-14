481868f607d8169fbb9a8e9ffc9ac2e04d518842
Luke Hoffmann <lakehoffmann@gmail.com>
2020-06-14 16:15:29 +0930

    Change jquery to vanilla JS
    Fade-in is not working

--- circlething.js
+++ circlething.js
@@ -1,4 +1,4 @@
-/* global $ */
+'use strict'
 
 var Circlething = function () {
   var columns = 3
@@ -7,33 +7,45 @@ var Circlething = function () {
   var colors = ['red', 'orange', 'pink', 'purple']
   var minimumMatch = 3
 
-  $(document).ready(function () {
-    $('#gameboard').css('width', columns * pieceSize + 'px')
-      .css('height', rows * pieceSize + 'px')
+  function ready (fn) {
+    if (document.readyState !== 'loading') {
+      fn()
+    } else {
+      document.addEventListener('DOMContentLoaded', fn)
+    }
+  }
+
+  ready(function () {
+    const board = document.querySelector('#gameboard')
+    board.style.width = columns * pieceSize + 'px'
+    board.style.height = rows * pieceSize + 'px'
+
+    board.addEventListener('mouseleave', () => clearClass('highlight'))
 
     startGame()
-
-    $('#gameboard').mouseleave(function () {
-      clearClass('highlight')
-    })
   })
 
   var startGame = function () {
+    document.querySelector('body').classList.remove('endgame')
+    document.querySelector('#gameboard').classList.remove('endgame')
     recolorTitle()
     recolorFavicon()
 
-    $('.gamepiece').remove()
-    $('.column').remove()
+    document.querySelectorAll('.gamepiece')
+      .forEach(e => e.parentElement.removeChild(e))
+    document.querySelectorAll('.column')
+      .forEach(e => e.parentElement.removeChild(e))
 
     var c, r, column, piece
     for (c = 1; c <= columns; c++) {
       column = newColumn(c)
-      column.appendTo('#gameboard')
+      document.querySelector('#gameboard').appendChild(column)
       for (r = 1; r <= rows; r++) {
         piece = newPiece(c, r)
-        piece.hide()
-          .appendTo(column)
-          .fadeIn('slow')
+        piece.classList.add('hide')
+        column.append(piece)
+        piece.classList.add('show')
+        piece.classList.remove('hide')
       }
     }
     while (detectEndgame()) {
@@ -42,51 +54,53 @@ var Circlething = function () {
   }
 
   var newColumn = function (c) {
-    return $('<div/>').addClass('column')
-      .attr('id', 'col' + c)
-      .css('width', pieceSize + 'px')
-      .css('height', rows * pieceSize + 'px')
+    var d = document.createElement('div')
+    d.classList.add('column')
+    d.setAttribute('id', 'col' + c)
+    d.style.width = pieceSize + 'px'
+    d.style.height = rows * pieceSize + 'px'
+    return d
   }
 
   var newPiece = function (c, r) {
     var color = randomColor() + 'gamepiece'
-    return $('<div/>').addClass('gamepiece')
-      .addClass(color)
-      .data('color', color)
-      .attr('id', pieceId(c, r))
-      .css('width', pieceSize + 'px')
-      .css('height', pieceSize + 'px')
-      .hover(pieceHover)
-      .click(pieceClick)
+    var d = document.createElement('div')
+    d.classList.add('gamepiece')
+    d.classList.add(color)
+    d.setAttribute('color', color)
+    d.setAttribute('id', pieceId(c, r))
+    d.style.width = pieceSize + 'px'
+    d.style.height = pieceSize + 'px'
+    d.addEventListener('mouseover', pieceHover)
+    d.addEventListener('click', pieceClick)
+    return d
   }
 
   var pieceHover = function () {
     clearClass('highlight')
-    if (addClassToAdjacentPieces($(this), 'highlight') < minimumMatch) {
+    if (addClassToAdjacentPieces(this, 'highlight') < minimumMatch) {
       clearClass('highlight')
     }
   }
 
   var pieceClick = function () {
-    if ($('body').hasClass('endgame')) {
+    if (document.querySelector('body').classList.contains('endgame')) {
       startGame()
-      $('body').removeClass('endgame')
-      $('#gameboard').removeClass('endgame')
     } else {
       clearClass('delete')
-      var toDelete = addClassToAdjacentPieces($(this), 'delete')
+      var toDelete = addClassToAdjacentPieces(this, 'delete')
       var removed = 0
-
+      console.log(`todelete ${toDelete}`)
       if (toDelete >= minimumMatch) {
-        $('.delete').attr('id', '')
-          .fadeOut('fast', function () {
-            this.remove()
-            removed++
-            if (removed === toDelete) {
-              rebuildIds()
-              detectEndgame()
-            }
-          })
+        document.querySelectorAll('.delete').forEach(e => {
+          e.removeAttribute('id')
+          e.parentNode.removeChild(e)
+          removed++
+          if (removed === toDelete) {
+            rebuildIds()
+            detectEndgame()
+          }
+        })
       }
     }
   }
@@ -97,14 +111,17 @@ var Circlething = function () {
         var above = r - 1
         while (above > 0 && !pieceExists(c, r)) {
           if (pieceExists(c, above)) {
-            getPiece(c, above).attr('id', pieceId(c, r))
+            getPiece(c, above).setAttribute('id', pieceId(c, r))
           }
           above--
         }
         if (!pieceExists(c, r)) {
-          newPiece(c, r).hide()
-            .prependTo($('#col' + c))
-            .fadeIn('slow')
+          var col = document.querySelector('#col' + c)
+          var piece = newPiece(c, r)
+          piece.classList.add('hide')
+          col.prepend(piece)
+          piece.classList.add('show')
+          piece.classList.remove('hide')
         }
       }
     }
@@ -121,19 +138,18 @@ var Circlething = function () {
         }
       }
     }
-    $('body').addClass('endgame')
-    $('#gameboard').addClass('endgame')
+    document.querySelector('body').classList.add('endgame')
+    document.querySelector('#gameboard').classList.add('endgame')
     return true
   }
 
   var addClassToAdjacentPieces = function (div, newClass, color) {
-    color = color || div.data('color')
-
-    if (div && div.data('color') === color && !div.hasClass(newClass)) {
+    color = color || div.getAttribute('color')
+    if (div && div.getAttribute('color') === color && !div.classList.contains(newClass)) {
       var c = +piecePos(div).column
       var r = +piecePos(div).row
 
-      div.addClass(newClass)
+      div.classList.add(newClass)
       return 1 +
             addClassToAdjacentPieces(getPiece(c, r - 1), newClass, color) +
             addClassToAdjacentPieces(getPiece(c, r + 1), newClass, color) +
@@ -145,15 +161,16 @@ var Circlething = function () {
   }
 
   var clearClass = function (className) {
-    $('.gamepiece').removeClass(className)
+    document.querySelectorAll('.gamepiece')
+      .forEach(e => e.classList.remove(className))
   }
 
   var getPiece = function (c, r) {
-    return $('#' + pieceId(c, r))
+    return document.querySelector(`[id="${pieceId(c, r)}"]`)
   }
 
   var pieceExists = function (c, r) {
-    return $('#' + pieceId(c, r)).length > 0
+    return document.querySelectorAll(`[id="${pieceId(c, r)}"]`).length > 0
   }
 
   var pieceId = function (c, r) {
@@ -161,7 +178,7 @@ var Circlething = function () {
   }
 
   var piecePos = function (div) {
-    var id = div.attr('id') || ''
+    var id = div.getAttribute('id') || ''
     var pos = []
     pos.column = +id.split('_')[0] || 0
     pos.row = +id.split('_')[1] || 0
@@ -174,14 +191,16 @@ var Circlething = function () {
 
   // re-generate random colors outside of gameboard
   var recolorTitle = function () {
-    $('.random').each(function () {
-      $(this).removeClass(colors.join(' '))
-      $(this).addClass(randomColor())
+    document.querySelectorAll('.random').forEach(e => {
+      for (const color of colors) {
+        e.classList.remove(color)
+      }
+      e.classList.add(randomColor())
     })
   }
 
   // debugging recolor
-  $('body').click(function () {
+  document.querySelector('body').click(function () {
     recolorTitle()
     recolorFavicon()
   })
@@ -189,7 +208,7 @@ var Circlething = function () {
   // randomise the favicon
   var recolorFavicon = function () {
     var ico = randomColor().concat('.png')
-    $('#randomfavicon').attr('href', ico)
+    document.querySelector('#randomfavicon').setAttribute('href', ico)
   }
 }
 
